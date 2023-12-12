1a04a755a553b894a5b44a014940846b0bc58ff7
Conor Walsh <conor.walsh@blackstone.com>
2023-12-12 08:40:16 -0500

    Adding day0 template dir

---
+++ src/day0/mod.rs
@@ -0,0 +1,9 @@
+
+
+pub fn solve1(file_path: &str) -> u32 {
+
+}
+
+pub fn solve2(file_path: &str) -> u32 {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::env;
-use std::process;
-
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {    
     println!("Day 1 Answer 0: {}", day1::solve(&"./src/day1/input.txt", 0));
@@ -14,4 +12,7 @@ fn main() {
 
     println!("Day 3 Answer 0: {}", day3::solve1(&"./src/day3/input.txt"));
     println!("Day 3 Answer 1: {}", day3::solve2(&"./src/day3/input.txt"));
+
+    println!("Day 4 Answer 0: {}", day4::solve1(&"./src/day4/input.txt"));
+    println!("Day 4 Answer 1: {}", day4::solve2(&"./src/day4/input.txt"));
 }
