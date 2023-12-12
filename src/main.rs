dac812507c6718b778a8a42c141c97d17b425069
Conor Walsh <conor@Conors-MacBook-Pro.local>
2023-12-12 00:06:11 -0500

    Day4s1

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 use std::env;
 use std::process;
 
-mod day1;
-mod day2;
-mod day3;
+// mod day1;
+// mod day2;
+// mod day3;
+mod day4;
 
 fn main() {    
-    println!("Day 1 Answer 0: {}", day1::solve(&"./src/day1/input.txt", 0));
-    println!("Day 1 Answer 1: {}", day1::solve(&"./src/day1/input.txt", 1));
+    // println!("Day 1 Answer 0: {}", day1::solve(&"./src/day1/input.txt", 0));
+    // println!("Day 1 Answer 1: {}", day1::solve(&"./src/day1/input.txt", 1));
 
-    println!("Day 2 Answer 0: {}", day2::solve1(&"./src/day2/input.txt"));
-    println!("Day 2 Answer 1: {}", day2::solve2(&"./src/day2/input.txt"));
+    // println!("Day 2 Answer 0: {}", day2::solve1(&"./src/day2/input.txt"));
+    // println!("Day 2 Answer 1: {}", day2::solve2(&"./src/day2/input.txt"));
 
-    println!("Day 3 Answer 0: {}", day3::solve1(&"./src/day3/input.txt"));
-    println!("Day 3 Answer 1: {}", day3::solve2(&"./src/day3/input.txt"));
+    // println!("Day 3 Answer 0: {}", day3::solve1(&"./src/day3/input.txt"));
+    // println!("Day 3 Answer 1: {}", day3::solve2(&"./src/day3/input.txt"));
+
+    // println!("Day 4 Answer 0: {}", day4::solve1(&"./src/day4/input.txt"));
+    println!("Day 4 Answer 1: {}", day4::solve2(&"./src/day4/input.txt"));
 }
