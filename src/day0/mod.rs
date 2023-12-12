f9f4f21a21826d6b3f45353d22714851d6c39a73
Conor Walsh <conor.walsh@blackstone.com>
2023-12-12 09:18:07 -0500

    adding scaffolding loops to template

--- src/day0/mod.rs
+++ src/day0/mod.rs
@@ -1,9 +1,37 @@
-
-
 pub fn solve1(file_path: &str) -> u32 {
+    let mut total = 0;
 
+    if let Ok(lines) = aoc23::read_lines(file_path) {
+      
+      let grid_length = aoc23::get_file_line_size(file_path);
+      // total = u32::try_from(grid_length).unwrap();
+      
+      for (j, line) in lines.enumerate() {
+        if let Ok(row) = line {
+          // println!("{}",row);
+          
+        }
+      }
+      
+    }
+    total
 }
 
 pub fn solve2(file_path: &str) -> u32 {
+    let mut total = 0;
 
+    if let Ok(lines) = aoc23::read_lines(file_path) {
+      
+      let grid_length = aoc23::get_file_line_size(file_path);
+      // total = u32::try_from(grid_length).unwrap();
+      
+      for (j, line) in lines.enumerate() {
+        if let Ok(row) = line {
+          // println!("{}",row);
+          
+        }
+      }
+      
+    }
+    total
 }
